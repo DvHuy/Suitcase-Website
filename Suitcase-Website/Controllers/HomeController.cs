4e50c8d9e69531d29b49052a05edbf28fe3f9c71
DvHuy <ahuyahihi4@gmail.com>
2024-09-01 10:53:49 +0700

    Build Product Detail by ViewBag

--- Suitcase-Website/Controllers/HomeController.cs
+++ Suitcase-Website/Controllers/HomeController.cs
@@ -36,6 +36,14 @@ namespace Suitcase_Website.Controllers
  
         }
 
+        public IActionResult ChiTietSanPham(String maSp)
+        {
+            var sanPham = db.TDanhMucSps.SingleOrDefault(x => x.MaSp == maSp);
+            var anhSanPham = db.TAnhSps.Where(x => x.MaSp == maSp).ToList();
+            ViewBag.anhSanPham = anhSanPham;
+            return View(sanPham);
+        }
+
         public IActionResult Privacy()
         {
             return View();
